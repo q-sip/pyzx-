ea7e28eca3a9562cda7c8feb332181d76d2c338d
Touko <touko.ristiranta@gmail.com>
2026-01-22 08:38:59 +0200

    fix vindex tests, maybe actions pass?

--- tests/test_graph_neo4j/test_vindex.py
+++ tests/test_graph_neo4j/test_vindex.py
@@ -1,32 +1,30 @@
-import unittest
 from pyzx.utils import VertexType
-from pyzx.graph.graph_neo4j import GraphNeo4j
 
-def test_vindex_empty():
-    g = GraphNeo4j(graph_id="test_empty")
+
+def test_vindex_empty(neo4j_graph_unit):
+    """Test that vindex returns 0 for empty graph"""
+    g = neo4j_graph_unit
     assert g.vindex() == 0
-    g.close()
 
-def test_vindex_after_creation():
-    g = GraphNeo4j(graph_id="test_creation")
+
+def test_vindex_after_creation(neo4j_graph_e2e):
+    """Test that vindex increments after creating vertices"""
+    g = neo4j_graph_e2e
     
-    vertices_data = [{"ty": VertexType.BOUNDARY, "qubit": 0, "row": 0},
+    vertices_data = [
+        {"ty": VertexType.BOUNDARY, "qubit": 0, "row": 0},
         {"ty": VertexType.Z, "qubit": 0, "row": 1},
-        {"ty": VertexType.X, "qubit": 0, "row": 2}]
+        {"ty": VertexType.X, "qubit": 0, "row": 2}
+    ]
     g.create_graph(vertices_data=vertices_data, edges_data=[])
     
     assert g.vindex() == 3
-    g.close()
 
 
-class TestNeo4jVindex(unittest.TestCase):
-    def setUp(self):
-        self.g = GraphNeo4j(graph_id="test_increment")
-
-    def tearDown(self):
-        self.g.close()
-
-    def test_vindex_increment(self):
-        initial = self.g.vindex()
-        self.g.create_graph(vertices_data=[{"ty": VertexType.Z, "qubit": 0, "row": 1}], edges_data=[])
-        self.assertEqual(self.g.vindex(), initial + 1)
+def test_vindex_increment(neo4j_graph_e2e):
+    """Test that vindex continues to increment correctly"""
+    g = neo4j_graph_e2e
+    
+    initial = g.vindex()
+    g.create_graph(vertices_data=[{"ty": VertexType.Z, "qubit": 0, "row": 1}], edges_data=[])
+    assert g.vindex() == initial + 1
