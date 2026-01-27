a7a95d2ff695d550a94148e4d8e11524ef96f6d2
OttoSFelix <otto.suokas@helsinki.fi>
2026-01-27 15:40:48 +0200

    type method added and tests for it

---
+++ tests/test_graph_neo4j/test_type.py
@@ -0,0 +1,44 @@
+# tests/test_graph_neo4j/test_type.py
+from pyzx.utils import VertexType
+
+from tests.test_graph_neo4j._base_unittest import Neo4jE2ETestCase, Neo4jUnitTestCase
+
+
+
+
+
+class TestTypeE2E(Neo4jE2ETestCase):
+    def test_type_empty(self):
+        g = self.g
+
+        vertices_data = [
+            {"ty": VertexType.BOUNDARY, "qubit": 0, "row": 0},
+            {"ty": VertexType.Z, "qubit": 0, "row": 1},
+            {"ty": VertexType.X, "qubit": 0, "row": 2},
+        ]
+        g.create_graph(vertices_data=vertices_data, edges_data=[])
+        self.assertEqual(g.type(3), None)
+
+    def test_type_X(self):
+        g = self.g
+
+        vertices_data = [
+            {"ty": VertexType.BOUNDARY, "qubit": 0, "row": 0},
+            {"ty": VertexType.Z, "qubit": 0, "row": 1},
+            {"ty": VertexType.X, "qubit": 0, "row": 2},
+        ]
+        g.create_graph(vertices_data=vertices_data, edges_data=[])
+
+        self.assertEqual(g.type(2), 2)
+
+    def test_type_BOUNDARY(self):
+        g = self.g
+
+        vertices_data = [
+            {"ty": VertexType.BOUNDARY, "qubit": 0, "row": 0},
+            {"ty": VertexType.Z, "qubit": 0, "row": 1},
+            {"ty": VertexType.X, "qubit": 0, "row": 2},
+        ]
+        g.create_graph(vertices_data=vertices_data, edges_data=[])
+
+        self.assertEqual(g.type(0), 0)
