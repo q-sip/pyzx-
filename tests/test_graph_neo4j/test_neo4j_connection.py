a94372e2d5b909bd8247bca12799d44d1bcf8ed3
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2026-02-04 01:46:07 +0200

    Added: basic test to see if envs are set.

---
+++ tests/test_graph_neo4j/test_neo4j_connection.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+
+class TestConnectionWithNeo4j(unittest.TestCase):
+
+    def test_has_envs(self):
+        uri = os.getenv("NEO4J_URI")
+        self.assertIsNotNone(uri)
+        user = os.getenv("NEO4J_USER")
+        self.assertIsNotNone(user)
+        password = os.getenv("NEO4J_PASSWORD")
+        self.assertIsNotNone(password)
+
+
+if __name__ == "__main__":
+    unittest.main()
