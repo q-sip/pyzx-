5af1c99feb30b0df3e2bafdd21e7413a3580121a
LouiseOdier <maarit.vilen@helsinki.fi>
2026-02-04 17:57:51 +0200

    demo tests

--- neo4j_functionality_test.py
+++ neo4j_functionality_test.py
@@ -6,6 +6,8 @@ from fractions import Fraction
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 from pyzx.simplify import full_reduce
+from pyzx.generate import cliffordT
+from pyzx.drawing import draw
 
 load_dotenv()
 
@@ -44,5 +46,23 @@ v_ids = g.create_graph(
     inputs=[0],
     outputs=[3],
 )
+
+def manually_construc_diagram():
+    g = GraphNeo4j
+    i = g.add_vertex(0,0,0)
+    v = g.add_vertex(1,0,1, Fraction(1,2))
+    w = g.add_vertex(2,0,2, Fraction(-1,2))
+    o = g.add_vertex(0,0,3)
+    g.add_edges([(i,v), (v,w),(w,o)])
+    #display(draw(g)) -> Neo4j call
+
+
+def generate_and_simplify_circuit():
+    g=cliffordT(3,20)
+    #display(draw(g)) -> Neo4j call
+    full_reduce(g)
+    #g.normalise()
+    draw(g)
+
 # For planning only
 #full_reduce(g: BaseGraph[VT,ET], matchf: Optional[Callable[[Union[VT, ET]],bool]]=None, quiet:bool=True, stats:Optional[Stats]=None) -> None:
