7b5ea75c9a6b7382878e5eba306687d8684984cf
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2026-02-04 02:05:53 +0200

    Added: dotenv loading to the test file... again...

---
+++ tests/test_graph_neo4j/__init__.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    import unittest
+    import sys
+    sys.path.append('..')
+    sys.path.append('.')
+    from dotenv import load_dotenv
+    load_dotenv(dotenv_path=".env.pyzx")
