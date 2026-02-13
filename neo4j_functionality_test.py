58f50cddd883ab72daa489ab540ca62428319191
alexisvarjo <alexisvarj@gmail.com>
2026-02-13 14:40:06 +0200

    print sorted poies :D

--- neo4j_functionality_test.py
+++ neo4j_functionality_test.py
@@ -1,11 +1,12 @@
 import os
+import random
 from fractions import Fraction
+
+import pyzx as zx
 from dotenv import load_dotenv
 from pyzx.graph.graph_neo4j import GraphNeo4j
 from pyzx.graph.graph_s import GraphS
-from pyzx.utils import VertexType, EdgeType
-import pyzx as zx
-import random
+from pyzx.utils import EdgeType, VertexType
 
 load_dotenv()
 
@@ -41,10 +42,7 @@ AUTH = (os.getenv("NEO4J_USER"), os.getenv("NEO4J_PASSWORD"))
 #     print(f"Lopullisessa graafissa: {g.num_vertices()} nodea, {g.num_edges()} kaarta")
 
 
-
-
-
-#MANUAALINEN GRAAFI TESTAUKSEEN!
+# MANUAALINEN GRAAFI TESTAUKSEEN!
 
 g = GraphNeo4j(
     uri=URI,
@@ -54,13 +52,15 @@ g = GraphNeo4j(
     graph_id="test_graph",
 )
 
+
 def graph_step_by_step():
     """Testaa graafin luomista askel askeleelta"""
-    i = g.add_vertex(0,0,0)
-    v = g.add_vertex(1,0,1, Fraction(1,2))
-    w = g.add_vertex(2,0,2, Fraction(-1,2))
-    o = g.add_vertex(0,0,3)
-    g.add_edges([(i,v), (v,w), (w,o)])
+    i = g.add_vertex(0, 0, 0)
+    v = g.add_vertex(1, 0, 1, Fraction(1, 2))
+    w = g.add_vertex(2, 0, 2, Fraction(-1, 2))
+    o = g.add_vertex(0, 0, 3)
+    g.add_edges([(i, v), (v, w), (w, o)])
+
 
 def large_graph():
     """Suurempi graafi"""
@@ -78,10 +78,10 @@ def large_graph():
         ],
         edges_data=[
             ((0, 1), EdgeType.SIMPLE),
-            ((1, 2), EdgeType.SIMPLE),      # Z-Z spider fusion candidate
-            ((2, 3), EdgeType.HADAMARD),    # Hadamard edge
-            ((3, 4), EdgeType.SIMPLE),      # X-X spider fusion candidate
-            ((4, 5), EdgeType.HADAMARD),    # Hadamard edge
+            ((1, 2), EdgeType.SIMPLE),  # Z-Z spider fusion candidate
+            ((2, 3), EdgeType.HADAMARD),  # Hadamard edge
+            ((3, 4), EdgeType.SIMPLE),  # X-X spider fusion candidate
+            ((4, 5), EdgeType.HADAMARD),  # Hadamard edge
             ((5, 6), EdgeType.SIMPLE),
             ((6, 7), EdgeType.SIMPLE),
             ((7, 8), EdgeType.SIMPLE),
@@ -90,6 +90,7 @@ def large_graph():
         outputs=[8],
     )
     # print(g.type(3))
+
 
 # def graph_full_reduce():
 #     """Neo4j graafin full reduce"""
@@ -100,10 +101,12 @@ def large_graph():
 # #graph_step_by_step()
 # graph_full_reduce()
 
+
 def iterable_graph_creation():
     num = 0
     choices = [VertexType.X, VertexType.Z]
     while True:
+        g.clear_graph()
         g.add_vertex(VertexType.BOUNDARY, 0, 0)
         g.add_vertex(VertexType.BOUNDARY, 0, 0)
         g.add_vertex(VertexType.Z, 0, 0)
@@ -120,10 +123,15 @@ def iterable_graph_creation():
         for x in range(num):
             g.add_vertex(random.choice(choices), 0, 0)
             g.add_vertex(random.choice(choices), 0, 0)
-            g.add_edge((random.randint(2, g.num_vertices()-1), random.randint(2, g.num_vertices()-1)))
+            g.add_edge(
+                (
+                    random.randint(2, g.num_vertices() - 1),
+                    random.randint(2, g.num_vertices() - 1),
+                )
+            )
         zx.full_reduce(g)
-        print(f'{num} succesful')
+        print(f"{num} succesful")
         num += 1
 
 
-print(sorted([100, 101, 65, 102, 103, 30, 104, 105, 106, 107, 31, 108, 109, 110, 111, 112, 84, 113, 85, 114, 115, 116, 117, 118, 41, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 79, 148, 149, 86, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 87, 163, 164, 165, 29, 166, 76, 88, 89, 167, 168, 169, 77, 81, 61, 82, 170, 95, 171, 60, 172, 173, 96, 174, 175, 176, 69, 90, 48, 91, 97, 99, 92, 32, 62, 98, 177, 93, 83, 39, 36, 178, 74, 0, 1, 2, 94, 3, 44, 57, 55, 35, 4, 5, 45, 40, 28, 46, 42, 6, 7, 8, 9, 64, 10, 11, 37, 43, 73, 56, 12, 13, 66, 14, 15, 38, 16, 80, 17, 67, 51, 18, 50, 59, 47, 34, 68, 19, 78, 20, 21, 71, 54, 22, 49, 23, 24, 25, 72, 33, 70, 52, 26, 58, 75, 27, 63, 53]))
+iterable_graph_creation()
