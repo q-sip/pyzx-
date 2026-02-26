bdc949f4377d86b65b4ee9263f86a66b09939205
OttoSFelix <otto.suokas@helsinki.fi>
2026-02-26 13:57:10 +0200

    small fixes

--- manual_neo4j/zxdb_functionality_test.py
+++ manual_neo4j/zxdb_functionality_test.py
@@ -7,9 +7,11 @@ from pyzx.graph.graph_memgraph import GraphMemgraph
 load_dotenv()
 URI = os.getenv("MEMGRAPH_URI")
 AUTH = (os.getenv("MEMGRAPH_USER"), os.getenv("MEMGRAPH_PASSWORD"))
+c = zx.generate.CNOT_HAD_PHASE_circuit(2, 20, seed=50)
+g = c.to_graph(backend='memgraph')
 zxdb = ZXdb(URI, AUTH[0], AUTH[1])
 path = zxdb.current_path
 def graph_full_reduce():
-    zxdb.spider_fusion(graph_id='graph_test_zxdb')
+    zxdb.spider_fusion()
 
 graph_full_reduce()
