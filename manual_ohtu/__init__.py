73cc3030bce86ca69cf3c16c8da39c8e0c3211e1
Touko Ristiranta <touko.ristiranta@gmail.com>
2026-04-16 19:17:15 +0300

    delete some things not needed in addon

--- manual_ohtu/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyzx import *
-
-if __name__ == "__main__":
-    print("This is just initer, have a nice day :)")
--- pyzx/web/__init__.py
+++
@@ -1,1 +0,0 @@
-from .compute import compute_pauli_webs, compute_stabilisers, compute_detecting_regions
