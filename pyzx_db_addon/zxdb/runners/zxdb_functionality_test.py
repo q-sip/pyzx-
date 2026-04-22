6674f075c4bbc69ec3a5cf7261caa828c2bdfafc
bladeefan2011 <eetosjar@gmail.com>
2026-04-22 22:30:19 +0300

    brought forgotten runner files back

---
+++ pyzx_db_addon/zxdb/runners/zxdb_functionality_test.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv
+import pyzx as zx
+from pyzx.simplify import spider_simp, to_gh
+from pyzx.graph.zxdb.zxdb import ZXdb
+
+# import random
+# from pyzx.graph.graph_memgraph import GraphMemgraph
+
+load_dotenv()
+URI = os.getenv("MEMGRAPH_URI")
+AUTH = (os.getenv("DB_USER"), os.getenv("DB_PASSWORD"))
+# for x in range(100):
+#     print(f'seed ===== {x}')
+c = zx.generate.CNOT_HAD_PHASE_circuit(3, 40, seed=43)
+# g = zx.generate.cliffordT(3, 30, seed=8, backend='memgraph')
+# c = g.copy(backend='simple')
+c_local = c.to_graph(backend='simple')
+g = c.to_graph(backend='memgraph')
+
+
+zxdb = ZXdb(URI, AUTH[0], AUTH[1])
+# path = zxdb.current_path
+print('starting full reduce...')
+print(f"Node count: {g.num_vertices()}")
+zxdb.full_reduce(g)
+zx.full_reduce(c_local)
+print('full reduce done!')
+print(f"Node count zxdb: {g.num_vertices()}")
+print(f'node count pyzx: {c_local.num_vertices()}')
+
+# # zx.full_reduce(s)
+# # s.normalize()
+# # 2. Now PyZX can safely read it
+g.normalize()
+print('normalize done')
+# # 3. Copy the graph to local memory (Simple backend) so the tensor calculation
+# # doesn't have to send 10,000 queries to Memgraph, which takes forever.
+# # zx.to_graph_like(g)
+g_local = g.copy(backend='simple')
+# print('g_local copy done')
+# # c_opt = zx.extract_circuit(g.clone())
+
+# 4. Compare the reduced graph directly to the original circuit!
+# print('started comparing')
+compare = zx.compare_tensors(g_local, c, preserve_scalar=False)
+print(f'Comparing: {compare}')
+# g = c.copy(backend="memgraph")
+
+
+# if False == False:
+#     print(f'False with seed {x}')
+#     break
+# zxdb.clear_all_data()
+# g.clear_clones()
+
+
+
+
---
+++ pyzx_db_addon/zxdb/runners/zxdb_functionality_test3.py
@@ -0,0 +1,109 @@
+import os
+from dotenv import load_dotenv
+import pyzx as zx
+from pyzx.simplify import spider_simp, to_gh
+from pyzx.graph.zxdb.zxdb import ZXdb
+
+# import random
+# from pyzx.graph.graph_memgraph import GraphMemgraph
+
+load_dotenv()
+URI = os.getenv("MEMGRAPH_URI")
+AUTH = (os.getenv("DB_USER"), os.getenv("DB_PASSWORD"))
+# for x in range(100):
+#     print(f'seed ===== {x}')
+c = zx.generate.CNOT_HAD_PHASE_circuit(6, 50, seed=45)
+# g = zx.generate.cliffordT(3, 30, seed=8, backend='memgraph')
+# c = g.copy(backend='simple')
+c_local = c.to_graph(backend='simple')
+g = c.to_graph(backend='memgraph')
+
+
+with ZXdb(URI, AUTH[0], AUTH[1]) as zxdb:
+
+
+    stop_requested = False
+
+
+    found = False
+
+    for q in range(3, 10):
+
+
+
+        if stop_requested or found:
+
+            break
+
+        for d in range(40, 100, 20):
+
+
+            if stop_requested or found:
+
+                break
+
+            for s in range(40, 100):
+
+                g = zx.generate.CNOT_HAD_PHASE_circuit(q, d, seed=s)
+                g = g.to_graph(backend="memgraph")
+                c = g.copy(backend='simple')
+
+                print(q, d, s, "vars")
+
+                print(f"Node count zxdb before: {g.num_vertices()}")
+
+                zxdb.full_reduce(g)
+
+                print('full reduce done!')
+
+                print(f"Node count zxdb: {g.num_vertices()}")
+
+
+                nodes = g.num_vertices()
+
+                g.normalize()
+
+                print('normalize done')
+
+
+        
+
+                print(f"Node count pyzx start: {c.num_vertices()}")
+
+
+                zx.full_reduce(c)
+
+
+                print('full reduce done!')
+
+
+
+                print(f"Node count pyzx: {c.num_vertices()}")
+                compare = zx.compare_tensors(g, c, preserve_scalar=False)
+                print(f'Comparing: {compare}')
+
+                if g.num_vertices() < c.num_vertices():
+                    print(" ")
+                    print(" ")
+                    print(" ")
+                    print("We beat pyzx")
+                    print(" ")
+                    print(" ")
+
+
+                #paause = input("s")
+                zxdb.clear_all_data()
+                g.remove_all_data()
+
+
+                if not compare:
+
+                    print('Comparison failed, stopping loop.')
+
+                    print(f"{q,d,s} failed")
+                    stop_requested = True
+                    break
+
+
+
+
